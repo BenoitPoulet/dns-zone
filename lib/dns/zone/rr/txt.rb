28679a56dafe87b0d5d97608302d0a3216ddd96d
Aaron Spettl <aaron@spettl.de>
2018-09-15 16:38:43 +0200

    Split long text in TXT/SPF records into chunks

--- lib/dns/zone/rr/txt.rb
+++ lib/dns/zone/rr/txt.rb
@@ -7,7 +7,7 @@ class DNS::Zone::RR::TXT < DNS::Zone::RR::Record
 
   def dump
     parts = general_prefix
-    parts << %Q{"#{text}"}
+    parts << quote_text(text)
     parts.join(' ')
   end
 
@@ -17,6 +17,19 @@ class DNS::Zone::RR::TXT < DNS::Zone::RR::Record
     # extract text from within quotes; allow multiple quoted strings; ignore escaped quotes
     @text = rdata.scan(/"#{DNS::Zone::RR::REGEX_STRING}"/).join
     self
+  end
+
+  protected
+
+  # Quotes the given text, e.g. the content of a TXT or SPF record.
+  # Respects the rule that a single string may contain at most 255 chars, but
+  # multiple strings can be used to produce longer content. See also RFC 4408,
+  # section 3.1.3.
+  #
+  # @param text [String] the (potentially long) text
+  # @return [String] the quoted string or, if needed, several quoted strings
+  def quote_text(text)
+    text.chars.each_slice(200).map(&:join).map { |chunk| %Q{"#{chunk}"} }.join(' ')
   end
 
 end
