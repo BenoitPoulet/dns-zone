37389ee3cbf9d1443a88ac41179b01a730af94c5
Benoit Poulet <benoit.poulet@businessdecision.com>
2020-01-22 12:31:42 +0100

    Fix CNAME for multiple ORIGIN

--- lib/dns/zone/rr/cname.rb
+++ lib/dns/zone/rr/cname.rb
@@ -15,6 +15,13 @@ class DNS::Zone::RR::CNAME < DNS::Zone::RR::Record
     rdata = load_general_and_get_rdata(string, options)
     return nil unless rdata
     @domainname = rdata
+    if !@domainname.end_with?(".")
+      if options[:last_origin] == '@'
+        @domainname = "#{@domainname}.#{options[:default_origin]}"
+      else
+        @domainname = "#{@domainname}.#{options[:last_origin]}"
+      end
+    end
     self
   end
 
